d96840cecea665b55bf6c3e5d26d539103254b0e
aldialfian19 <105551380+aldialfian19@users.noreply.github.com>
2022-11-06 17:48:32 +0800

    Initial Commit

---
+++ ArRobot/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ArRobot
+//
+//  Created by Rinaldi Alfian on 06/11/22.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
